863eeecf6be896da102f8082de2c571f56f3521f
Jeiler Vargas <jeiler.a.vargas@gmail.com>
2024-09-20 17:49:27 -0500

    Segundo commit

--- src/main/java/pe/edu/cibertec/patitas_frontend_a/controller/LoginController.java
+++ src/main/java/pe/edu/cibertec/patitas_frontend_a/controller/LoginController.java
@@ -1,9 +1,28 @@
 package pe.edu.cibertec.patitas_frontend_a.controller;
 
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pe.edu.cibertec.patitas_frontend_a.viewmodel.LoginModel;
 
 @Controller
-@
+@RequestMapping("/login")
 public class LoginController {
+
+    @GetMapping("/inicio")
+    public String inicio(Model model){
+        LoginModel loginModel = new LoginModel("00","","");
+        model.addAttribute("nombre", "Pepe");
+        return "inicio";
+    }
+
+    @PostMapping("/autenticar")
+    public String autenticar(Model model){
+
+        LoginModel loginModel = new LoginModel("00","","Jeiler Vargas");
+    }
 }
